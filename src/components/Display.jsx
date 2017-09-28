d94560f29e0e725cea43c78a4d0125010a9ccd0d
jacob ruleaux <jakeruleaux@gmail.com>
2017-09-28 10:05:28 -0700

    begin display comp

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,6 +1,33 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
-function Display(){
-  return()
+const Display = ({ dispatch, flight}) => {
+  const { origin, destination, adults, children, date } = flight;
+  return (
+    <div>
+      <h1>Flight details</h1>
+      <hr/>
+      <h1>{origin}</h1>
+      <h1>{destination}</h1>
+      <h1>{adults}</h1>
+      <h1>{children}</h1>
+      <h1>{date}</h1>
+    </div>
+  );
+};
+
+Display.propTypes = {
+  flight: PropTypes.object,
+  origin: PropTypes.string,
+  destination: PropTypes.string,
+  adults: PropTypes.string,
+  children: PropTypes.string,
+  date: PropTypes.string,
+  dispatch: PropTypes.func
+};
+
+const mapStateToProps = state => {
+  const flight = state
 }
 export default Display;
